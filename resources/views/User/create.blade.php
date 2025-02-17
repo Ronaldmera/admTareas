1f431b0c221d20ed1d1bef29e7179a61df4e1fe0
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-17 18:10:13 -0500

    new user registration form

--- resources/views/User/create.blade.php
+++ resources/views/User/create.blade.php
@@ -4,34 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     @vite(['resources/css/user/create.css'])
-    <title>Registro usuarios</title>
 </head>
 
-<body class="d-flex justify-content-center align-items-center vh-100 ">
-    <div class="container mt-4">
-        <h1 class="text-center">Regístrate en ADM Task</h1>
-        <form action="{{ route('user.store') }}" method="POST" class="mt-4">
+<body>
+
+    <div class="form-container">
+        <p class="title">Regístrate en ADM Task</p>
+        <form class="form" action="{{ route('user.store') }}" method="POST">
             @csrf
-
-            <div class="form-group mb-3">
-                <label for="name">Nombre</label>
-                <input type="text" name="name" id="name" class="form-control" required>
-            </div>
-
-            <div class="form-group mb-3">
-                <label for="email">Correo Electrónico</label>
-                <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-
-            <div class="form-group mb-3">
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" id="password" class="form-control" required>
-            </div>
-
-            <button type="submit" class="btn btn-primary w-100">Crear</button>
+            <input type="text" class="input" placeholder="Nombre" name="name" required>
+            <input type="email" class="input" placeholder="Email" name="email" required>
+            <input type="password" class="input" placeholder="Password" name="password"required>
+            <button class="form-btn" type="submit">Registrarme</button>
+            <a class="form-cancel" href="{{ route('user.login') }}">Cancelar</a>
         </form>
     </div>
 
