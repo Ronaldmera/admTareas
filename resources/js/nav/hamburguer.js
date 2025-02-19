52d1607596ea8052e075d61798dc7f908a3624ee
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-19 18:52:39 -0500

    cerrar hamburguesa al dar click afuera

--- resources/js/nav/hamburguer.js
+++ resources/js/nav/hamburguer.js
@@ -5,3 +5,10 @@ hamburguer.addEventListener('click', () => {
     enlaces.classList.toggle('show');
 
 });
+// Cerrar el menú si se hace clic fuera de él
+document.addEventListener("click", function (event) {
+    if (!enlaces.contains(event.target) && !hamburguer.contains(event.target)) {
+        enlaces.classList.remove("show");
+    }
+});
+
