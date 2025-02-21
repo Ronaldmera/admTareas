ca38878f9be5c4bcf7c448c9a3dfb5e1e8bed31e
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-21 18:25:47 -0500

    footer responsive

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,11 @@
+<footer>
+    <div class="footer-container">
 
+        <div class="footer-title">
+            <h1>ADM Taks</h1>
+        </div>
+        <div class="footer-text">
+            <p>© 2025 ADM Tasks. Todos los derechos reservados.</p>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title', 'ADM Tasks')</title>
     @vite(['resources/css/nav/style.css'])
     @vite(['resources/css/global.css'])
+    @vite(['resources/css/footer/style.css'])
     @vite(['resources/js/nav/hamburguer.js'])
 </head>
 
