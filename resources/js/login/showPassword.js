9dfeb8f669bd03dec3ea7988e0ca91e9abd660d8
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-21 20:22:45 -0500

    icon visible and not visible login

--- resources/js/login/showPassword.js
+++ resources/js/login/showPassword.js
@@ -1,14 +1,17 @@
 let ico = document.querySelector(".eye-icon");//icono de ojo
+const iconoCloseEye = document.querySelector(".close-eye");
 let passwordContent = document.getElementById("password");//contenido del input password
 
-//al tocar el icono se ejecuta esta funcion flecha
+//mustra la contraseña y cambia el icono al de ojo con la linea cruzada
 ico.addEventListener('click', () => {
-    if(passwordContent.type === "password"){//si el tipo es password lo convierte en text
         passwordContent.type = "text";
-    }
-    else{
-        passwordContent.type = "password";//si el tipo es text lo convierte en password
-    }
-
-})
+        iconoCloseEye.style.display = "flex";
+        ico.style.display = "none";
+});
+//oculta la contraseña o la convierte en  tipo password y se coloca el icono de ojo
+iconoCloseEye.addEventListener('click', () => {
+    passwordContent.type = "password";
+    iconoCloseEye.style.display = "none";
+    ico.style.display = "flex";
+});
 
