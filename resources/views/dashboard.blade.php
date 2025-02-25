c28af126ebe6ed92536a3d7a426cd3f4b6dfc673
Ronaldmera <ronaldalme1230@gmail.com>
2025-02-24 19:00:41 -0500

    update...

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,27 +6,32 @@
         <div class="pending-tasks">
             <h2> Tareas Pendientes</h2>
             <div class="table-responsive">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th>Descripción</th>
-                            <th>Estado</th>
-                            <th>Fecha de Creación</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($pendingTasks as $pendingTask)
+                @if ($pendingTasks->isEmpty())
+                    <p class="no-tasks-message">No hay tareas pendientes.</p>
+                @else
+                    <table>
+                        <thead>
                             <tr>
-                                <td class="item">{{ $pendingTask->title }}</td>
-                                <td class="content">{{ $pendingTask->content }}</td>
-                                <td>{{ $pendingTask->status }}</td>
-                                <td> {{ $pendingTask->created_at->format('d-m-Y') }}</td>
+                                <th>Título</th>
+                                <th>Descripción</th>
+                                <th>Estado</th>
+                                <th>Fecha de Creación</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($pendingTasks as $pendingTask)
+                                <tr>
+                                    <td class="item">{{ $pendingTask->title }}</td>
+                                    <td class="content">{{ $pendingTask->content }}</td>
+                                    <td>{{ $pendingTask->status }}</td>
+                                    <td> {{ $pendingTask->created_at->format('d-m-Y') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
             </div>
+
         </div>
     </div>
 @endsection
