d2b41d3cb3f33f006becc2fbce47ee445d96386b
Ronaldmera <ronaldalme1230@gmail.com>
2025-03-20 16:09:02 -0500

    open modal animation in update profile view

--- resources/js/profile/app.js
+++ resources/js/profile/app.js
@@ -7,13 +7,13 @@ const imageInput = document.getElementById("image");
 let originalName = nameInput.value; // Guardar el nombre original antes de abrir el modal
 
 icoEdit.addEventListener("click", function () {
-    modal.style.display = "flex";
+    modal.classList.add('modal-edit-active');
     originalName = nameInput.value;
 });
 
 // Cerrar modal y restaurar el nombre original, pero eliminar la imagen seleccionada
 closeIco.addEventListener("click", function () {
-    modal.style.display = "none";
+    modal.classList.remove('modal-edit-active');
 
     nameInput.value = originalName;
     imageInput.value = "";
