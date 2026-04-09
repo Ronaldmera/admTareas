555fe204599be9460ef334e4fd427df37eb908dd
Ronaldmera <ronaldalme1230@gmail.com>
2026-04-09 16:21:20 -0500

    eliminando borde de btn nav

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,34 +1,3 @@
-{{-- <header>
-    <div class="container">
-        <!-- Logo o Nombre de la Aplicación -->
-        <div class="logo">
-            <a href="{{ route('dashboard') }}">
-                <h1>ADM Taks</h1>
-            </a>
-        </div>
-        <nav>
-            <ul class="nav-links" id="nav-links">
-                <li><a href="{{ route('dashboard') }}">Inicio</a></li>
-                <li><a href="{{ route('task.index') }}">Tareas</a></li>
-                <li><a href="{{ route('user.showProfile') }}">Perfil</a></li>
-                <!-- Botón de Cerrar Sesión con Ícono -->
-                <li class="logout">
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="logout-btn">
-                            <img src="{{ asset('images/logout/logout.svg') }}" alt="Cerrar sesión" class="logout-img">
-                        </button>
-                    </form>
-                </li>
-            </ul>
-            <div class="hamburguer" id="hamburguer">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-        </nav>
-    </div>
-</header> --}}
 <div class="container-fluid  fixed-top  p-0">
     <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
@@ -43,7 +12,7 @@
                         <span class="text-white me-0">Salir</span>
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="btn">
+                            <button type="submit" class="btn border-0">
                                 <i class="bi bi-box-arrow-right text-light fs-5"></i>
                             </button>
                         </form>
