b5b9f9ffd5e094ecf4b7f3ed2673ccd489c24942
Ronaldmera <ronaldalme1230@gmail.com>
2026-04-10 15:39:57 -0500

    forzar HTTPS en URLs para corregir contenido mixto en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -18,9 +19,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {    
         Paginator::useBootstrap();
+         if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
 
