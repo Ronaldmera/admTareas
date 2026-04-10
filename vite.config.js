6a6df1e58e77d43ebbe8f230d9a66e7929a28a14
Ronaldmera <ronaldalme1230@gmail.com>
2026-04-10 14:35:45 -0500

    as

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-        'resources/css/login/style.css',
-        'resources/css/login/login_error.css',
-      ],
+      input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
   ],
